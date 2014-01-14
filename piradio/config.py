ac009abfa05f0637b4bd0e68565ed02c82de598b
Pedro Ferreira <pedro@dete.st>
2014-01-14 22:08:05 +0100

    Split config

---
+++ piradio/config.py
@@ -0,0 +1,10 @@
+
+# Disable debug mode.
+DEBUG = False
+
+# Secret key for session management. You can generate random strings here:
+# http://clsc.net/tools-old/random-string-generator.php
+SECRET_KEY = 'OVERRIDE THIS PLEASE'
+
+MPLAYER_SOCKET_FILE = '/tmp/mplayer.sock'
+MPLAYER_CMD = 'mplayer'
